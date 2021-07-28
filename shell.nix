043cdf36be063bb4086f6e3184e2e0d1b5eeeaa5
gege251 <gege251@gmail.com>
2021-07-28 16:11:38 +0900

    update nix

--- shell.nix
+++ shell.nix
@@ -1,15 +1,16 @@
-let sources = import ./nix/sources.nix;
-in with import sources.nixpkgs { };
-stdenv.mkDerivation {
-  name = "szabo-gergely-portfolio";
-  buildInputs = with elmPackages; [
-    elm
-    elm-live
-    elm-format
-    elm-analyse
-    elm-test
-    nodePackages.uglify-js
-    elm2nix
-    haskellPackages.mustache
-  ];
-}
+let
+  sources = import ./nix/sources.nix;
+in
+  with import sources.nixpkgs {};
+  mkShell {
+    buildInputs = with elmPackages; [
+      elm
+      elm-live
+      elm-format
+      elm-analyse
+      elm-test
+      nodePackages.uglify-js
+      elm2nix
+      haskellPackages.mustache
+    ];
+  }
